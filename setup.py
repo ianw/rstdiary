ab048076f79c738bc696160a5e74def2f1d8b71c
Ian Wienand <iwienand@redhat.com>
2024-10-29 14:51:54 +1100

    Bump to v3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ The output is chunked into months and lightly styled with bootstrap.
 
 setup(
     name='rstdiary',
-    version='2.1',
+    version='3.0',
     author='Ian Wienand',
     author_email='ian@wienand.org',
     packages=find_packages(),
